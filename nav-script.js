f654b2edc733a92e0248d6091d9d504af76555e0
Jorciney <jorcineyadorador@hotmail.com>
2017-05-23 18:10:59 +0200

    Added bootstrap and changed navbar

--- nav-script.js
+++ nav-script.js
@@ -1,34 +1,58 @@
 // animation
 jQuery.noConflict()(function () {
-    $('#nav').on('click', 'a', function () {
-        $('#content-placeholder').hide(600);
-        // fade in new selected subcontent
-        $('#content-placeholder').show(600);
-    });
+	$('#nav').on('click', 'a', function () {
+
+		//         Animation: fadeIn
+		$('#content-placeholder').css({
+			"opacity": "0",
+			"display": "block",
+		}).delay(250).show().animate({
+			opacity: 1
+		});
+
+	});
+});
+jQuery(document).ready(function ($) {
+	$("#menu-toggle").click(function (e) {
+		e.preventDefault();
+		$("#wrapper").toggleClass("active");
+		
+//		Here we are going to change the icon of #menu-toggle depending on the active class at that momment.
+		var classList = $('#main_icon').attr('class').split(/\s+/);
+		$.each(classList, function (index, item) {
+			if (item === 'glyphicon-align-justify') {
+				$('#main_icon').removeClass('glyphicon-align-justify');
+				$('#main_icon').addClass('glyphicon-chevron-left');
+				
+				//align the nav text to the center when menu is shown
+				$('.main-nav li a').css('text-align', 'center');
+				
+				$('#logo').show();
+			}
+			//When the menu is open
+			if (item === 'glyphicon-chevron-left') {
+				$('#main_icon').removeClass('glyphicon-chevron-left');
+				$('#main_icon').addClass('glyphicon-align-justify');
+				
+				//align the nav text to the left when menu is hidden
+				$('.main-nav li a').css('text-align', 'left');
+				
+				$('#logo').hide();
+			}
+		});
+	});
+
 });
 
+
 function loadServicesContent() {
-    document.getElementById('content-placeholder').innerHTML = '<object type=\'text/html\' style="min-width:100%; min-height: 101%; overflow:hidden; height:100%;"  data=\'services.html\' ></object>';
+	document.getElementById('content-placeholder').innerHTML = '<object type=\'text/html\' style="min-width:100%; min-height: 101%; overflow:hidden; height:100%;"  data=\'services.html\' ></object>';
 }
 
 function loadGalleryContent() {
-    document.getElementById('content-placeholder').innerHTML = '<object type=\'text/html\' style="min-width:100%; min-height: 101%; overflow:hidden;height:100%;" data=\'gallery.html\' ></object>';
+	document.getElementById('content-placeholder').innerHTML = '<object type=\'text/html\' style="min-width:100%; min-height: 101%; overflow:hidden;height:100%;" data=\'gallery.html\' ></object>';
 }
 
 function loadHomeContent() {
-    document.getElementById('content-placeholder').innerHTML = '<object type=\'text/html\' style="min-width:100%; min-height: 101%; overflow:hidden height:100%;" data=\'index.html\' ></object>';
+	document.getElementById('content-placeholder').innerHTML = '<object type=\'text/html\' style="min-width:100%; min-height: 101%; overflow:hidden height:100%;" data=\'index.html\' ></object>';
 }
-
-// $('#nav-gallery').click(function(){
-//        $('html, body').animate({
-//            scrollTop: $('#section-gallery').position().top },
-//            1000
-//        );
-//    });
-
-//$(function () {
-//    $("#nav a").click(function (e) {
-//        e.preventDefault();
-//        $('html,body').scrollTo(this.hash, this.hash);
-//    });
-//});
--- script.js
+++ script.js
@@ -1,11 +1,3 @@
 $.get("navbar.html", function (data) {
-        $("#nav-placeholder").replaceWith(data);
-    });  
-
-
-//$.get("navbar.html", function (data) {
-//        $("#nav-placeholder").replaceWith(data);
-//    });
-
-
-
+    $("#nav-placeholder").replaceWith(data);
+});
