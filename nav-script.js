ff4c437edec55b519f1eed8b2a56744c008c37f6
Jorciney Dias Chaveiro <jorcineyadorador@hotmail.com>
2017-05-24 00:25:51 +0200

    Modified nav style

--- nav-script.js
+++ nav-script.js
@@ -1,58 +1,71 @@
 // animation
 jQuery.noConflict()(function () {
-	$('#nav').on('click', 'a', function () {
+    $('#sidebar').on('click', 'a', function () {
 
-		//         Animation: fadeIn
-		$('#content-placeholder').css({
-			"opacity": "0",
-			"display": "block",
-		}).delay(250).show().animate({
-			opacity: 1
-		});
+        //         Animation: fadeIn
+        $('#content-placeholder').css({
+            "opacity": "0",
+            "display": "block",
+        }).delay(250).show().animate({
+            opacity: 1
+        });
 
-	});
+    });
 });
 jQuery(document).ready(function ($) {
-	$("#menu-toggle").click(function (e) {
-		e.preventDefault();
-		$("#wrapper").toggleClass("active");
-		
-//		Here we are going to change the icon of #menu-toggle depending on the active class at that momment.
-		var classList = $('#main_icon').attr('class').split(/\s+/);
-		$.each(classList, function (index, item) {
-			if (item === 'glyphicon-align-justify') {
-				$('#main_icon').removeClass('glyphicon-align-justify');
-				$('#main_icon').addClass('glyphicon-chevron-left');
-				
-				//align the nav text to the center when menu is shown
-				$('.main-nav li a').css('text-align', 'center');
-				
-				$('#logo').show();
-			}
-			//When the menu is open
-			if (item === 'glyphicon-chevron-left') {
-				$('#main_icon').removeClass('glyphicon-chevron-left');
-				$('#main_icon').addClass('glyphicon-align-justify');
-				
-				//align the nav text to the left when menu is hidden
-				$('.main-nav li a').css('text-align', 'left');
-				
-				$('#logo').hide();
-			}
-		});
-	});
+    $('#nav').on('click', 'a', function () {
+
+        //         Animation: fadeIn
+        $('#content-placeholder').css({
+            "opacity": "0",
+            "display": "block",
+        }).delay(250).show().animate({
+            opacity: 1
+        });
+
+    });
+    $("#menu-toggle").click(function (e) {
+        e.preventDefault();
+        $("#wrapper").toggleClass("active");
+
+        //		Here we are going to change the icon of #menu-toggle depending on the active class at that momment.
+        var classList = $('#main_icon').attr('class').split(/\s+/);
+        $.each(classList, function (index, item) {
+            if (item === 'glyphicon-align-justify') {
+                $('#main_icon').removeClass('glyphicon-align-justify');
+                $('#main_icon').addClass('glyphicon-chevron-left');
+
+                //align the nav text to the center when menu is shown
+                $('.main-nav li a').css('text-align', 'center');
+                $('.main-nav li').css('margin', '0px auto 0px auto');
+
+                $('#logo').show();
+            }
+            //When the menu is open
+            if (item === 'glyphicon-chevron-left') {
+                $('#main_icon').removeClass('glyphicon-chevron-left');
+                $('#main_icon').addClass('glyphicon-align-justify');
+
+                //align the nav text to the left when menu is hidden
+                $('.main-nav li a').css('text-align', 'left');
+                $('.main-nav li').css('margin', '-20px');
+
+                $('#logo').hide();
+            }
+        });
+    });
 
 });
 
 
 function loadServicesContent() {
-	document.getElementById('content-placeholder').innerHTML = '<object type=\'text/html\' style="min-width:100%; min-height: 101%; overflow:hidden; height:100%;"  data=\'services.html\' ></object>';
+    document.getElementById('content-placeholder').innerHTML = '<object type=\'text/html\' style="min-width:100%; min-height: 101%; overflow:hidden; height:100%;"  data=\'services.html\' ></object>';
 }
 
 function loadGalleryContent() {
-	document.getElementById('content-placeholder').innerHTML = '<object type=\'text/html\' style="min-width:100%; min-height: 101%; overflow:hidden;height:100%;" data=\'gallery.html\' ></object>';
+    document.getElementById('content-placeholder').innerHTML = '<object type=\'text/html\' style="min-width:100%; min-height: 101%; overflow:hidden;height:100%;" data=\'gallery.html\' ></object>';
 }
 
 function loadHomeContent() {
-	document.getElementById('content-placeholder').innerHTML = '<object type=\'text/html\' style="min-width:100%; min-height: 101%; overflow:hidden height:100%;" data=\'index.html\' ></object>';
+    document.getElementById('content-placeholder').innerHTML = '<object type=\'text/html\' style="min-width:100%; min-height: 101%; overflow:hidden height:100%;" data=\'index.html\' ></object>';
 }
