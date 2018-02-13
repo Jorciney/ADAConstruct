c8af83e5f129758c5853d57f0eb9f1446fbb1d39
Jorciney Dias Chaveiro <jorcineyadorador@hotmail.com>
2018-02-13 20:54:28 +0100

    Refactoring the side menu to use https://slideout.js.org/

--- nav-script.js
+++ nav-script.js
@@ -1,4 +1,12 @@
+var slideout;
 $(document).ready(function () {
+    slideout = new Slideout({
+        'panel': document.getElementById('my-panel'),
+        'menu': document.getElementById('menu'),
+        'padding': 200,
+        'tolerance': 30,
+        'easing': 'cubic-bezier(.32,2,.55,.27)'
+    });
 
     setTimeout(function () {
         $('body').addClass('loaded');
@@ -7,7 +15,17 @@ $(document).ready(function () {
 
 });
 
+window.onload = function () {
+    if (slideout && screen.width >= 720){
+        slideout.toggle();
+    }
+};
+
 jQuery(function ($) {
+
+    $('.toggle-button').on('click', function () {
+        slideout.toggle();
+    });
 
     $('#sidebar').on('click', 'a', function () {
         //         Animation: fadeIn
@@ -63,6 +81,24 @@ jQuery(function ($) {
     $('.js-show-sidedrawer').on('click', showSidedrawer);
     $('.js-hide-sidedrawer').on('click', hideSidedrawer);
 });
+//
+// function close(eve) {
+//     eve.preventDefault();
+//     slideout.close();
+// }
+//
+// slideout
+//     .on('beforeopen', function() {
+//         this.panel.classList.add('panel-open');
+//     })
+//     .on('open', function() {
+//         this.panel.addEventListener('click', close);
+//     })
+//     .on('beforeclose', function() {
+//         this.panel.classList.remove('panel-open');
+//         this.panel.removeEventListener('click', close);
+//     });
+
 
 function loadServicesContent() {
     $('#content-placeholder').load('services.html');
